850edc169b29ec79967908077b1510b7d4b5323c
Mashikawa Hideaki <kotakoma@masukawaeimeis-MacBook-Pro.local>
2019-12-02 00:26:49 +0900

    Automatic_updating
    自動更新のバグ修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,8 +48,8 @@ $(document).on('turbolinks:load', function(){
   });
   // 自動更新
   $(function(){
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var reloadMessages = function() {
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
         last_message_id = $('.message:last').data("message-id");
           $.ajax({
             url: "api/messages",
@@ -63,16 +63,14 @@ $(document).on('turbolinks:load', function(){
               messages.forEach(function(message){
                 insertHTML = buildHTML(message);
                 $('.messages').append(insertHTML);
+                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
               })
-              $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
           })
           .fail(function() {
             alert('自動更新に失敗しました');
           });
-        }
-        setInterval(reloadMessages, 7000);
     }else{
-      console.log('更新しない')
-    };
-  })
+    }};
+    setInterval(reloadMessages, 5000);
+  });
 });
